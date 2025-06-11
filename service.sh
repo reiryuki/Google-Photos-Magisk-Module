a93e5363e51098e80ed188cb422d35251048731d
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2025-06-11 15:25:48 +0700

    v0.2

--- service.sh
+++ service.sh
@@ -32,19 +32,7 @@ else
 fi
 
 # function
-grant_permission() {
-pm grant $PKG android.permission.READ_EXTERNAL_STORAGE
-pm grant $PKG android.permission.WRITE_EXTERNAL_STORAGE
-if [ "$API" -ge 29 ]; then
-  pm grant $PKG android.permission.ACCESS_MEDIA_LOCATION 2>/dev/null
-  appops set $PKG ACCESS_MEDIA_LOCATION allow
-fi
-if [ "$API" -ge 33 ]; then
-  pm grant $PKG android.permission.READ_MEDIA_AUDIO
-  pm grant $PKG android.permission.READ_MEDIA_VIDEO
-  pm grant $PKG android.permission.READ_MEDIA_IMAGES
-  appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
-fi
+appops_set() {
 appops set $PKG LEGACY_STORAGE allow
 appops set $PKG READ_EXTERNAL_STORAGE allow
 appops set $PKG WRITE_EXTERNAL_STORAGE allow
@@ -54,6 +42,9 @@ appops set $PKG READ_MEDIA_IMAGES allow
 appops set $PKG WRITE_MEDIA_AUDIO allow
 appops set $PKG WRITE_MEDIA_VIDEO allow
 appops set $PKG WRITE_MEDIA_IMAGES allow
+if [ "$API" -ge 29 ]; then
+  appops set $PKG ACCESS_MEDIA_LOCATION allow
+fi
 if [ "$API" -ge 30 ]; then
   appops set $PKG MANAGE_EXTERNAL_STORAGE allow
   appops set $PKG NO_ISOLATED_STORAGE allow
@@ -61,6 +52,9 @@ if [ "$API" -ge 30 ]; then
 fi
 if [ "$API" -ge 31 ]; then
   appops set $PKG MANAGE_MEDIA allow
+fi
+if [ "$API" -ge 33 ]; then
+  appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
 fi
 if [ "$API" -ge 34 ]; then
   appops set $PKG READ_MEDIA_VISUAL_USER_SELECTED allow
@@ -84,8 +78,9 @@ fi
 # grant
 PKG=com.google.android.apps.photos
 if appops get $PKG > /dev/null 2>&1; then
+  pm grant --all-permissions $PKG
   appops set $PKG SYSTEM_ALERT_WINDOW allow
-  grant_permission
+  appops_set
 fi
 
 
